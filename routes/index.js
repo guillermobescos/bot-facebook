da52c13c0bc1f2cd612bd60ff4486fe930cbb254
Vincent Langlet <vincentl@theodo.fr>
2017-05-11 16:55:47 +0200

    LUUL

--- routes/index.js
+++ routes/index.js
@@ -71,6 +71,14 @@ function receivedMessage(event) {
 
         // If we receive a text message, check to see if it matches a keyword
         // and send back the example. Otherwise, just echo the text we received.
+        switch (messageText) {
+            case 'generic':
+                sendGenericMessage(senderID);
+                break;
+            case 'Coucou':
+                sendTextMessage(senderID, 'salut');
+                break;
+            default:
                 sendTextMessage(senderID, messageText);
         }
     } else if (messageAttachments) {
